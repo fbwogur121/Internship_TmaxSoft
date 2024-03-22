083245263fb5e0507c6db514658c486da7406bf6
parkhs21 <2001phs92@gmail.com>
2024-03-22 14:34:10 +0900

    refactor: chagne Message data class (to dto)

--- src/main/kotlin/com/Tmax/refactorCodeGpt/dto/request/ChatGptRequest.kt
+++ src/main/kotlin/com/Tmax/refactorCodeGpt/dto/request/ChatGptRequest.kt
@@ -1,22 +1,17 @@
 package com.Tmax.refactorCodeGpt.dto.request
 
+import com.Tmax.refactorCodeGpt.dto.Message
+
 data class ChatGptRequest(
     val model: String = "gpt-3.5-turbo",
-    val messages: Array<Message>,
-    val maxTokens: Int = 500,
-    val temperature: Int = 0
+    val messages: List<Message>,
 ) {
-    data class Message(
-        val role: String = "user",
-        val content: String
-    )
-
     companion object {
         fun of(fileExtension: String, code: String): ChatGptRequest =
-            ChatGptRequest(messages = arrayOf(makePrompt(fileExtension, code)))
+            ChatGptRequest(messages = listOf(makePrompt(fileExtension, code)))
 
         private fun makePrompt(fileExtension: String, code: String): Message =
-            Message(content = """
+            Message(role = "user", content = """
                 You role is perfect code refactoring prompt.
                 Refactor the following code for better readability and maintainability.
                 Don't say ANY explain. Just response the code strictly.
