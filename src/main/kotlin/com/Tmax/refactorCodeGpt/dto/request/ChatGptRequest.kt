827bceba23d0507c5d2b5b315f1cfa9457fde7bd
parkhs21 <2001phs92@gmail.com>
2024-03-22 14:09:28 +0900

    fix: solve 404 error - change gpt request body

--- src/main/kotlin/com/Tmax/refactorCodeGpt/dto/request/ChatGptRequest.kt
+++ src/main/kotlin/com/Tmax/refactorCodeGpt/dto/request/ChatGptRequest.kt
@@ -1,18 +1,22 @@
 package com.Tmax.refactorCodeGpt.dto.request
 
 data class ChatGptRequest(
-    val model: String = "text-davinci-003",
-    val prompt: String,
+    val model: String = "gpt-3.5-turbo",
+    val messages: Array<Message>,
     val maxTokens: Int = 500,
     val temperature: Int = 0
 ) {
+    data class Message(
+        val role: String = "user",
+        val content: String
+    )
 
     companion object {
         fun of(fileExtension: String, code: String): ChatGptRequest =
-            ChatGptRequest(prompt = makePrompt(fileExtension, code))
+            ChatGptRequest(messages = arrayOf(makePrompt(fileExtension, code)))
 
-        private fun makePrompt(fileExtension: String, code: String): String =
-            """
+        private fun makePrompt(fileExtension: String, code: String): Message =
+            Message(content = """
                 You role is perfect code refactoring prompt.
                 Refactor the following code for better readability and maintainability.
                 Don't say ANY explain. Just response the code strictly.
@@ -23,6 +27,6 @@ data class ChatGptRequest(
                 ```
 
                 Respond start with the line 'Code:'
-            """.trimIndent()
+            """.trimIndent())
     }
 }
