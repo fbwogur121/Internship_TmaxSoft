1d35ca8e3f13c1f4c442117ad6488f98b662a713
fbwougr121 <fbwogur121@naver.com>
2024-03-25 17:50:55 +0900

    improve prompt

--- src/main/kotlin/com/Tmax/refactorCodeGpt/dto/request/ChatGptRequest.kt
+++ src/main/kotlin/com/Tmax/refactorCodeGpt/dto/request/ChatGptRequest.kt
@@ -13,7 +13,9 @@ data class ChatGptRequest(
         private fun makePrompt(fileExtension: String, code: String): Message =
             Message(role = "user", content = """
                 You role is perfect code refactoring prompt.
+                Refactoring has the effect of improving the readability of the code and reducing its complexity, and these benefits are derived from a simpler, cleaner, or more expressive internal architecture or object model to improve the maintainability of the source code and improve scalability. allows you to create
                 Refactor the following code for better readability and maintainability.
+                However, you must restructure the code without changing the results.
                 Don't say ANY explain. Just response the code strictly.
                 This code's file extension: $fileExtension
                 Here is the code:
